a35c83c81bec124dfa22a3a1ddf04daf13dcd1ae
adelaney11 <adam.delaney11@gmail.com>
2020-04-28 18:36:21 -0500

    Work on models

--- flaskDemo/models.py
+++ flaskDemo/models.py
@@ -14,8 +14,10 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __table__ = db.Model.metadata.tables['User']
+    '''
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     # email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
@@ -25,8 +27,11 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.name}', '{self.image_file}')"
+    '''
 
 class Expense(db.Model):
+    __table__ = db.Model.metadata.tables['Expense']
+    '''
     __table_args__ = {'extend_existing': True}
     expenseId = db.Column(db.Integer, primary_key=True)
     expenseType = db.Column(db.String(20), nullable=False)
@@ -34,22 +39,27 @@ class Expense(db.Model):
 
     def __repr__(self):
         return f"Expense('{self.expenseType}', '{self.totalAmount}')"
-
+    '''
 
 class Payment(db.Model):
+    __table__ = db.Model.metadata.tables['Payment']
+    
+    '''
     __table_args__ = {'extend_existing': True}
     paymentId = db.Column(db.Integer, primary_key=True)
     expenseId = db.Column(db.Integer, db.ForeignKey('expense.expenseId'), nullable=False)
-    userId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('user.userId'), nullable=False)
     description = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"Payment('{self.amount}', '{self.description}', '{self.date}')"
-
+    '''
 
 class Budget(db.Model):
+    __table__ = db.Model.metadata.tables['Budget']
+    '''
     __table_args__ = {'extend_existing': True}
     budgetId = db.Column(db.Integer, primary_key=True)
     budgetType = db.Column(db.String(20), nullable=False)
@@ -59,6 +69,7 @@ class Budget(db.Model):
 
     def __repr__(self):
         return f"Budget('{self.budgetName}', '{self.budgetType}', '{self.amount}', '{self.dateRange}')"
+    '''
 
 class BudgetsFor(db.Model):
     __table__ = db.Model.metadata.tables['BudgetsFor']
